72c4dc6f40ccfdda98d20df6eec1ae9dc2ffc819
mihaicc30 <alemihai25@gmail.com>
2022-08-18 10:00:02 +0100

    fixed routing* extra work

--- config/cors.js
+++
@@ -1,7 +0,0 @@
-// function corsMiddleware(req, res, next) {
-//     res.set('Access-Control-Allow-Origin', '*');
-//     res.cookie('cookieName', 'cookieValue', { sameSite: 'none', secure: true})
-//     next();
-// }
-
-// module.exports = corsMiddleware;
